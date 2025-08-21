8236f40831638be15b9f3c1be3bf122ae86f77ba
Gustavo Ottoboni <ottoguz87@gmail.com>
2025-08-20 21:25:39 -0300

    feat: :sparkles: adds new task component

---
+++ src/app/components/tasks/new-task/new-task.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-new-task',
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.scss'
+})
+export class NewTaskComponent {
+  @Output()
+  cancel = new EventEmitter<void>();
+
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
+
+
+  onCancel() {
+    this.cancel.emit();
+  }
+}
