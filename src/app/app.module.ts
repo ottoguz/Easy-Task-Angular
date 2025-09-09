cea8b53b242a8ea3604d201612dad64afbce2a7e
Gustavo Ottoboni <ottoguz87@gmail.com>
2025-09-08 21:09:47 -0300

    refactor: :recycle: adds tasks module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,32 +2,25 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { UserComponent } from "./components/user/user.component";
-import { TasksComponent } from "./components/tasks/tasks.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
-import { TaskComponent } from "./components/tasks/task/task.component";
-import { NewTaskComponent } from "./components/tasks/new-task/new-task.component";
-import { FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { SharedModule } from "./shared/shared.module";
-
+import { TasksModule } from "./components/tasks/tasks.module";
 
 @NgModule({
     declarations: [
         AppComponent, 
         HeaderComponent,
         UserComponent,
-        TasksComponent,
-        TaskComponent,
-        NewTaskComponent,
     ],
     bootstrap: [AppComponent],
     imports: [
         BrowserModule, 
         RouterModule,
-        FormsModule,
         CommonModule,
         SharedModule,
+        TasksModule
     ]
 })
 export class AppModule {
